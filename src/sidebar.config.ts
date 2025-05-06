af0a7699ce20fa3258c6c2c13223d1fc95537bb8
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2025-05-06 11:31:28 +0200

    Fix sidebar collapsing while parsing

--- src/sidebar.config.ts
+++ src/sidebar.config.ts
@@ -1,14 +1,17 @@
 import { z } from "astro/zod"
+import type { PartialSpecificKeys } from "./utils/types";
 
 const sidebarItem = z.object({
   label: z.string(),
   autogenerate: z.object({
     directory: z.string()
     }).optional(),
-  collapsed: z.boolean().default(true).optional(), 
+  collapsed: z.boolean().default(true), 
 })
 
-export type SidebarItem = z.infer<typeof sidebarItem>;
+export const sidebarItems = z.array(sidebarItem).default([])
+
+export type SidebarItem = PartialSpecificKeys<z.infer<typeof sidebarItem>, 'collapsed'>;
 
 export const sidebarConfig: readonly SidebarItem[] = [
   {
